e916e058cfa920b73263f6f6cb801e6e1147e331
Davi Lucciola <davilucciola14@gmail.com>
2025-05-20 12:18:46 -0300

    [module4/class-4] - updating next config to export ssg

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  trailingSlash: true, // Transform sub-html in subfolders with index.html
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "raw.githubusercontent.com",
-        pathname: "**",
-      },
-    ],
+    // remotePatterns: [
+    //   {
+    //     protocol: "https",
+    //     hostname: "raw.githubusercontent.com",
+    //     pathname: "**",
+    //   },
+    // ],
+    loader: "custom",
+    loaderFile: "./src/app/images.js",
   },
 };
 
